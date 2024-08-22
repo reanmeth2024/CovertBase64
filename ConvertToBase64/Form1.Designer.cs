8d15d4b720554f884607144d0658bf50e1a18770
reanmeth2024 <renameth2024@hotmail.com>
2024-08-22 06:36:33 -0700

    convert

---
+++ ConvertToBase64/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace ConvertToBase64
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnChange64 = new Button();
+            lbbase64 = new Label();
+            txbBase64 = new TextBox();
+            ChangePNG = new Button();
+            label1 = new Label();
+            txtInBase64 = new RichTextBox();
+            SuspendLayout();
+            // 
+            // btnChange64
+            // 
+            btnChange64.Location = new Point(12, 12);
+            btnChange64.Name = "btnChange64";
+            btnChange64.Size = new Size(75, 23);
+            btnChange64.TabIndex = 0;
+            btnChange64.Text = "To Base64";
+            btnChange64.UseVisualStyleBackColor = true;
+            btnChange64.Click += btnChange64_Click;
+            // 
+            // lbbase64
+            // 
+            lbbase64.AutoSize = true;
+            lbbase64.Location = new Point(93, 16);
+            lbbase64.Name = "lbbase64";
+            lbbase64.Size = new Size(90, 15);
+            lbbase64.TabIndex = 4;
+            lbbase64.Text = "Xuất mã Base64";
+            // 
+            // txbBase64
+            // 
+            txbBase64.Location = new Point(199, 13);
+            txbBase64.Multiline = true;
+            txbBase64.Name = "txbBase64";
+            txbBase64.Size = new Size(223, 23);
+            txbBase64.TabIndex = 5;
+            // 
+            // ChangePNG
+            // 
+            ChangePNG.Location = new Point(12, 53);
+            ChangePNG.Name = "ChangePNG";
+            ChangePNG.Size = new Size(75, 23);
+            ChangePNG.TabIndex = 6;
+            ChangePNG.Text = "To PNG";
+            ChangePNG.UseVisualStyleBackColor = true;
+            ChangePNG.Click += ChangePNG_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(93, 57);
+            label1.Name = "label1";
+            label1.Size = new Size(95, 15);
+            label1.TabIndex = 8;
+            label1.Text = "Nhập Mã Base64";
+            // 
+            // txtInBase64
+            // 
+            txtInBase64.Location = new Point(199, 53);
+            txtInBase64.Name = "txtInBase64";
+            txtInBase64.Size = new Size(570, 272);
+            txtInBase64.TabIndex = 9;
+            txtInBase64.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(txtInBase64);
+            Controls.Add(label1);
+            Controls.Add(ChangePNG);
+            Controls.Add(txbBase64);
+            Controls.Add(lbbase64);
+            Controls.Add(btnChange64);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnChange64;
+        private Label lbbase64;
+        private TextBox txbBase64;
+        private Button ChangePNG;
+        private Label label1;
+        private RichTextBox txtInBase64;
+    }
+}
---
+++ ConvertToBase64/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace ConvertToBase64
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string ImageToBase64(Image image, System.Drawing.Imaging.ImageFormat format)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                image.Save(ms, format);
+                byte[] imageBytes = ms.ToArray();
+                return Convert.ToBase64String(imageBytes);
+            }
+        }
+        private void btnChange64_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "PNG Files|*.png";
+            openFileDialog.Title = "Select a PNG file";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                Image image = Image.FromFile(filePath);
+                string base64String = ImageToBase64(image, System.Drawing.Imaging.ImageFormat.Png);
+                txbBase64.Text = base64String;
+            }
+        }
+        private Image Base64ToImage(string base64String)
+        {
+            byte[] imageBytes = Convert.FromBase64String(base64String);
+            using (MemoryStream ms = new MemoryStream(imageBytes))
+            {
+                return Image.FromStream(ms);
+            }
+        }
+        private void ChangePNG_Click(object sender, EventArgs e)
+        {
+            string base64String = txtInBase64.Text;
+            if (string.IsNullOrWhiteSpace(base64String))
+            {
+                MessageBox.Show("Please enter or paste a Base64 string.");
+                return;
+            }
+
+            try
+            {
+                // Chuyển đổi Base64 thành ảnh
+                Image image = Base64ToImage(base64String);
+
+                // Mở hộp thoại để chọn nơi lưu tệp ảnh
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "PNG Files|*.png";
+                saveFileDialog.Title = "Save Image File";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Lưu ảnh vào tệp
+                    string filePath = saveFileDialog.FileName;
+                    image.Save(filePath, System.Drawing.Imaging.ImageFormat.Png);
+                    MessageBox.Show("Image saved successfully!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
